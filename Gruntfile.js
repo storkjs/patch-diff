14ffc1829ca849b72fc8b511596f68355ea425cc
sagiegurari <sagiegurari@gmail.com>
2015-12-10 13:11:26 +0200

    coverage of tests in travis build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,43 @@
 /*eslint no-sync: 0*/
 
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-mocha-test');
+    require('load-grunt-tasks')(grunt);
 
     grunt.config.init({
+        clean: {
+            options: {
+                force: true
+            },
+            dot: 'true',
+            target: {
+                src: [
+                    'target/**'
+                ]
+            }
+        },
+        copy: {
+            coverage: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['index.js'],
+                        dest: 'target/coverage'
+                    },
+                    {
+                        expand: true,
+                        src: ['tests/**'],
+                        dest: 'target/coverage'
+                    }
+                ]
+            }
+        },
+        blanket: {
+            full: {
+                files: {
+                    'target/coverage/lib': ['lib/']
+                }
+            }
+        },
         mochaTest: {
             full: {
                 options: {
@@ -12,11 +46,36 @@ module.exports = function (grunt) {
                     reporter: 'spec'
                 },
                 src: ['tests/**/*test.js']
+            },
+            coverageLCOV: {
+                options: {
+                    require: 'blanket',
+                    reporter: 'mocha-lcov-reporter',
+                    quiet: true,
+                    captureFile: 'target/coverage/report/coverage.info'
+                },
+                src: ['./target/coverage/tests/*test.js']
+            }
+        },
+        coveralls: {
+            options: {
+                force: true
+            },
+            full: {
+                src: 'target/coverage/report/*.info'
             }
         }
     });
 
-    grunt.registerTask('test', [
+    grunt.registerTask('localtest', [
         'mochaTest:full'
+    ]);
+
+    grunt.registerTask('test', [
+        'clean:target',
+        'copy:coverage',
+        'blanket:full',
+        'mochaTest:coverageLCOV',
+        'coveralls:full'
     ]);
 };
