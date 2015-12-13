c93f18ed3139c3a908c7e6eeec87a14f5ed40eeb
sagiegurari <sagie.gurari@ebs.com>
2015-12-13 09:04:31 +0200

    add delete test cases

--- tests/merge-diff-test.js
+++ tests/merge-diff-test.js
@@ -7,183 +7,195 @@
 var MergeDiff = require('../');
 var assert = require('assert');
 
-function createBaseObject() {
-    return {
-        string: "string",
-        number: 5,
-        boolean: true,
-        array: [0, 1, '2', false, {prop: 'val'}],
-        obj: {
-            property: "value1",
-            property2: "value2"
-        },
-        nested: {
-            a: {
-                b: {
-                    c: {
-                        d: {
-                            property: "value1",
-                            property2: "value2"
+describe("merge-diff", function () {
+    function createBaseObject() {
+        return {
+            string: "string",
+            number: 5,
+            boolean: true,
+            array: [0, 1, '2', false, {prop: 'val'}],
+            obj: {
+                property: "value1",
+                property2: "value2"
+            },
+            nested: {
+                a: {
+                    b: {
+                        c: {
+                            d: {
+                                property: "value1",
+                                property2: "value2"
+                            }
                         }
                     }
                 }
             }
-        }
-        /*
-         func: function () {
-         return "hello";
-         }
-         */
-    };
-}
+            /*
+             func: function () {
+             return "hello";
+             }
+             */
+        };
+    }
 
-describe("merge", function () {
-    describe('merging the number 20 to the path "string"', function () {
+    describe("merge", function () {
+        describe('merging the number 20 to the path "string"', function () {
 
-        var merger = new MergeDiff(createBaseObject());
-        var expectedObject = createBaseObject();
+            var merger = new MergeDiff(createBaseObject());
+            var expectedObject = createBaseObject();
 
-        expectedObject.string = 20;
+            expectedObject.string = 20;
 
-        it('merged object should be the same as expected object', function () {
-            merger.merge(20, 'string');
-            assert.deepEqual(merger.object, expectedObject);
-        });
-
-
-    });
-
-    describe('merging the string "test" to 3 existing properties with no path (to root)', function () {
-
-        var merger = new MergeDiff(createBaseObject());
-        var originalObject = createBaseObject();
-
-
-        it('merged object should be the same as expected object', function () {
-
-            merger.merge({
-                string: "test",
-                boolean: "test",
-                number: "test"
+            it('merged object should be the same as expected object', function () {
+                merger.merge(20, 'string');
+                assert.deepEqual(merger.object, expectedObject);
             });
 
-            assert.equal(merger.object.string, 'test');
-            assert.equal(merger.object.boolean, 'test');
-            assert.equal(merger.object.number, 'test');
-            assert.notEqual(merger.object.array, 'test');
-            assert.deepEqual(merger.object.nested, originalObject.nested);
         });
 
-    });
+        describe('merging the string "test" to 3 existing properties with no path (to root)', function () {
 
+            var merger = new MergeDiff(createBaseObject());
+            var originalObject = createBaseObject();
 
-    describe('merging 2 string properties and a new object with no path (to root)', function () {
+            it('merged object should be the same as expected object', function () {
 
-        var merger = new MergeDiff(createBaseObject());
+                merger.merge({
+                    string: "test",
+                    boolean: "test",
+                    number: "test"
+                });
 
-        it('merged object should be the same as expected object', function () {
-
-            merger.merge({
-                string: "test",
-                boolean: false,
-                newobject: {"test": "test"}
+                assert.equal(merger.object.string, 'test');
+                assert.equal(merger.object.boolean, 'test');
+                assert.equal(merger.object.number, 'test');
+                assert.notEqual(merger.object.array, 'test');
+                assert.deepEqual(merger.object.nested, originalObject.nested);
             });
 
-            assert.equal(merger.get('string'), 'test');
-            assert.equal(merger.object.boolean, false);
-            assert.deepEqual(merger.object.newobject, {test: 'test'});
         });
 
-    });
+        describe('merging 2 string properties and a new object with no path (to root)', function () {
 
+            var merger = new MergeDiff(createBaseObject());
 
-    describe('merging 4 items to array existing, modified, new and deleted', function () {
+            it('merged object should be the same as expected object', function () {
 
-        var merger = new MergeDiff(createBaseObject());
+                merger.merge({
+                    string: "test",
+                    boolean: false,
+                    newobject: {"test": "test"}
+                });
 
-        it('1 item should be modified 1 deleted and one added', function () {
-
-            merger.on("array", function (diff) {
-                assert.deepEqual(diff.differences, {'2': 'test', 4: merger.options.deleteKeyword, 7: "test2"});
+                assert.equal(merger.get('string'), 'test');
+                assert.equal(merger.object.boolean, false);
+                assert.deepEqual(merger.object.newobject, {test: 'test'});
             });
 
-            merger.merge({
-                array: {1: 1, '2': 'test', 4: merger.options.deleteKeyword, 7: "test2"}
+        });
+
+        describe('merging 4 items to array existing, modified, new and deleted', function () {
+
+            var merger = new MergeDiff(createBaseObject());
+
+            it('1 item should be modified 1 deleted and one added', function () {
+
+                merger.on("array", function (diff) {
+                    assert.deepEqual(diff.differences, {'2': 'test', 4: merger.options.deleteKeyword, 7: "test2"});
+                });
+
+                merger.merge({
+                    array: {1: 1, '2': 'test', 4: merger.options.deleteKeyword, 7: "test2"}
+                });
+
+                //[0, 1, '2', false, {prop: 'val'}]
+                //[0, 1, 'test', false, null, null, 'test2]
+
+                assert.deepEqual(merger.object.array, [0, 1, 'test', false, , , 'test2']);
             });
 
-            //[0, 1, '2', false, {prop: 'val'}]
-            //[0, 1, 'test', false, null, null, 'test2]
-
-            assert.deepEqual(merger.object.array, [0, 1, 'test', false, , , 'test2']);
         });
 
-    });
+        describe('delete a string and boolean', function () {
 
+            var merger = new MergeDiff(createBaseObject());
 
-    describe('delete a string and boolean', function () {
+            it('merged object should be the same as expected object', function () {
 
-        var merger = new MergeDiff(createBaseObject());
+                merger.merge({
+                    boolean: merger.options.deleteKeyword,
+                    string: merger.options.deleteKeyword
+                });
 
-        it('merged object should be the same as expected object', function () {
-
-            merger.merge({
-                boolean: merger.options.deleteKeyword,
-                string: merger.options.deleteKeyword
             });
 
-
         });
 
-    });
+        describe('merge nested object to "nested.a"', function () {
 
-    describe('merge nested object to "nested.a"', function () {
+            var merger = new MergeDiff(createBaseObject());
 
-        var merger = new MergeDiff(createBaseObject());
+            it('merged object should be the same as expected object', function () {
 
-        it('merged object should be the same as expected object', function () {
+                merger.on("nested", function (diff) {
+                    console.log('diff', diff.differences);
+                });
 
-            merger.on("nested", function (diff) {
-                console.log('diff', diff.differences);
+                merger.merge({
+                    b: {'added': 'asd', c: {d: {property2: undefined}}}
+                }, 'nested.a');
+
+                //[0, 1, '2', false, {prop: 'val'}]
+                //[0, 1, 'test', false, null, null, 'test2]
+
+                //expect(merger.object).property('array').to.deep.equal([0, 1, 'test', false, undefined, undefined, 'test2']);
             });
 
-            merger.merge({
-                b: {'added': 'asd', c: {d: {property2: undefined}}}
-            }, 'nested.a');
-
-            //[0, 1, '2', false, {prop: 'val'}]
-            //[0, 1, 'test', false, null, null, 'test2]
-
-            //expect(merger.object).property('array').to.deep.equal([0, 1, 'test', false, undefined, undefined, 'test2']);
         });
 
     });
 
+    describe("override", function () {
+    });
 
-});
+    describe("delete", function () {
+        describe('primitives', function () {
 
-describe("override", function () {});
+            function createPrimitives() {
+                return {
+                    boolean: true,
+                    number: 5,
+                    string: 'hello',
+                    'null': null
+                };
+            }
 
-describe("delete", function () {
-    describe('primitives', function () {
+            it('delete 1 primitive', function () {
+                var merger = new MergeDiff(createBaseObject());
+                var expectedObject = createBaseObject();
+                delete expectedObject.number;
 
-        function createPrimitives() {
-            return {
-                boolean: true,
-                number: 5,
-                string: 'hello',
-                'null': null
-            };
-        }
+                merger.delete('number');
 
-        describe('delete 1 primitive', function () {
-            var merger = new MergeDiff(createPrimitives());
-            //assert()
+                assert.deepEqual(merger.object, expectedObject);
+            });
+
         });
 
+        describe('nested', function () {
+            it('delete 1 nested object', function () {
+                var merger = new MergeDiff(createBaseObject());
+                var expectedObject = createBaseObject();
+                delete expectedObject.nested.a.b.c;
+
+                merger.delete('nested.a.b.c');
+
+                assert.deepEqual(merger.object, expectedObject);
+            });
+        });
     });
-});
 
-describe("differences events", function () {
+    describe("differences events", function () {
 
-
+    });
 });
