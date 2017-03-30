b5a0342953bf37fa819a8a7b3ab9cb31567585f6
Barak Edry <barak.edry@ebs.com>
2017-03-31 00:33:14 +0300

    added usage example

--- examples/example.js
+++ examples/example.js
@@ -30,3 +30,29 @@ merger.merge([1, 2, 3], 'myRoot.array');
 merger.merge([1, 2, 3, 4], 'myRoot.array');
 merger.merge({1: merger.options.deleteKeyword}, 'myRoot.array'); // splice the item at index 1
 console.log('final2', merger.get());
+
+
+
+// sync 2 objects with merger
+let obj1 = {};
+let obj2 = {};
+
+let merger1 = new MergeDiff(obj1);
+let merger2 = new MergeDiff(obj2);
+
+// handle root changes
+merger1.on('PATH:myRoot', (diff) => {
+    // patch obj2 with differences from obj1
+    merger2.merge(diff.differences, 'myRoot');
+});
+
+
+// modify obj1;
+merger1.merge({prop: 'changed'}, 'myRoot.item');
+merger1.merge({prop2: 'changed'}, 'myRoot.item2');
+merger1.merge({array: [1,2,3]}, 'myRoot.item3');
+merger1.merge({1: merger.options.deleteKeyword}, 'myRoot.array'); // splice the item at index 1
+merger1.override({prop2: {newobj: 'new property'}}, 'myRoot.item2'); // splice the item at index 1
+
+console.log('obj1=', JSON.stringify(obj1, 4, 4));
+console.log('obj2=', JSON.stringify(obj2, 4, 4));
