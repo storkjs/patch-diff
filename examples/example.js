a5558f8748775400ad23c8922d02b84b2392271f
Barak Edry <barak.edry@ebs.com>
2017-03-31 00:20:33 +0300

    added usage example

--- examples/example.js
+++ examples/example.js
@@ -9,18 +9,19 @@ let merger =  new MergeDiff({
     test: 'asd'
 });
 
-
+// add event handler for changes on path myRoot.item
 merger.on('PATH:myRoot.item', (diff) => {
    console.log('myRoot.item change', diff);
 });
 
 merger.merge({myRoot: {item: {prop: 'added'}}});
 merger.merge({prop: 'changed'}, 'myRoot.item');
-merger.override({myRoot: {item: { aDifferentProp: 'hi'}}});
-merger.merge({item: {aDifferentProp: merger.options.deleteKeyword}}, 'myRoot'); // splice the item at index 1
+merger.override({myRoot: {item: { aDifferentProp: 'hi'}}}); // override enforce a specific state
+merger.merge({item: {aDifferentProp: merger.options.deleteKeyword}}, 'myRoot'); // delete aDifferentProp key
 console.log('final', merger.get());
 
 
+// add event handler for changes on path myRoot.array
 merger.on('PATH:myRoot.array', (diff) => {
     console.log('myRoot.array change', diff);
 });
