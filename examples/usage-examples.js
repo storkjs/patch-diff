625a1cf10c9690d66e5d65fb8948273b054ccd15
Barak Edry <barak.edry@ebs.com>
2017-04-01 15:11:21 +0300

    cleanup for strictier eslint

--- examples/usage-examples.js
+++ examples/usage-examples.js
@@ -1,17 +1,20 @@
 /**
  * Created by barakedry on 30/03/2017.
  */
+/* eslint object-curly-spacing: 'off'*/
+/* eslint object-curly-newline: 'off'*/
+/* eslint no-console: 'off'*/
 'use strict';
 
 const PatchDiff = require('../index.js');
 
-let patcher =  new PatchDiff({
+let patcher = new PatchDiff({
     test: 'asd'
 });
 
 // add event handler for changes on path myRoot.item
 patcher.on('PATH:myRoot.item', (diff) => {
-   console.log('myRoot.item change', diff);
+    console.log('myRoot.item change', diff);
 });
 
 patcher.apply({myRoot: {item: {prop: 'added'}}});
@@ -32,7 +35,6 @@ patcher.apply({1: patcher.options.deleteKeyword}, 'myRoot.array'); // splice the
 console.log('final2', patcher.get());
 
 
-
 // sync 2 objects with patcher
 let obj1 = {};
 let obj2 = {};
@@ -50,26 +52,26 @@ patcher1.on('PATH:myRoot', (diff) => {
 // modify obj1;
 patcher1.apply({prop: 'changed'}, 'myRoot.item');
 patcher1.apply({prop2: 'changed'}, 'myRoot.item2');
-patcher1.apply({array: [1,2,3]}, 'myRoot.item3');
+patcher1.apply({array: [1, 2, 3]}, 'myRoot.item3');
 patcher1.apply({1: patcher.options.deleteKeyword}, 'myRoot.array'); // splice the item at index 1
 patcher1.override({prop2: {newobj: 'new property'}}, 'myRoot.item2'); // splice the item at index 1
 
 console.log('obj1=', JSON.stringify(obj1, 4, 4));
 console.log('obj2=', JSON.stringify(obj2, 4, 4));
 
-
+// proxy usage
 const PatcherProxy = require('../lib/patcher-proxy');
 
 let pp = PatcherProxy.create(patcher1, 'myRoot');
 
-console.log('obj2 before proxy changes',  JSON.stringify(obj2, 4, 4));
-pp.item2.prop2 = 'test';
-pp.item3.aaa = 'tes2';
-pp.item3.bbb = 'test3';
-delete pp.item3.bbb;
-delete pp.item.prop;
+console.log('obj2 before proxy changes', JSON.stringify(obj2, 4, 4));
+pp.item2.prop2 = 'test'; // change value of existing property
+pp.item3.aaa = 'tes2';  // define and assign new property to existing object
+pp.item3.bbb = 'test3';  // define and assign new property to existing object
+delete pp.item3.bbb; // delete property that didn't exists before
+delete pp.item.prop; // delete existing property
 setTimeout(() => {
-    console.log('obj2 after proxy changes',  JSON.stringify(obj2, 4, 4));
+    console.log('obj2 after proxy changes', JSON.stringify(obj2, 4, 4));
     pp.item2 = {
         prop: 'overriden'
     };
