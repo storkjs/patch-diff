31a81f932b03e489aa1a94c33cbe7f978c2e0519
Barak Edry <barak.edry@ebs.com>
2017-12-13 12:35:26 +0200

    added .at() .on() and .subscribe()

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
  * Created by barakedry on 6/19/15.
  */
 'use strict';
-module.exports = require('./lib/patch-diff');
+const PatchDiff = require('./lib/patch-diff');
+
+global.PatchDiff = PatchDiff;
+
+module.exports = PatchDiff;
